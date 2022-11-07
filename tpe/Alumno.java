0ea6437847064f14d0feb4336ac1da3e01ae47f4
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-11-07 17:53:15 -0300

    Agrega la clase alumno al tpe

---
+++ tpe/Alumno.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class Alumno implements Comparable<Alumno> {
+    
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private int edad;
+    private ArrayList<String> intereses;
+
+    public Alumno(String nombre, String apellido, String dni, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+        this.intereses = new ArrayList<>();
+    }
+
+    //Funcionalidades
+    public void addInteres(String interes) {
+        if (!this.intereses.contains(interes)) {
+            this.intereses.add(interes);
+        }
+    }
+
+    @Override
+    public int compareTo(Alumno otroAlumno) {
+        int resultado = this.getApellido().compareToIgnoreCase(otroAlumno.getApellido());
+        if (resultado == 0) {
+            resultado = this.getNombre().compareToIgnoreCase(otroAlumno.getNombre());
+        }
+        if (resultado == 0) {
+            resultado = this.getDni().compareToIgnoreCase(otroAlumno.getDni());
+        }
+        return resultado;
+    }
+
+    //Getters
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public String getDni() {
+        return this.dni;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        try {
+            Alumno otroAlumno = (Alumno) o;
+            return this.getNombre().equalsIgnoreCase(otroAlumno.getNombre()) && this.getApellido().equalsIgnoreCase(otroAlumno.getApellido());
+        }
+        catch(Exception exc) {
+            return false;
+        }
+    }
+}
--- tpe/Main.java
+++ tpe/Main.java
@@ -6,46 +6,52 @@ public class Main {
         ComportamientoAddAscendente add_ascendente = new ComportamientoAddAscendente();
         ComportamientoAddDescendente add_descendente = new ComportamientoAddDescendente();
 
-        ListaVinculada lista_vinculada = new ListaVinculada(nodo_raiz, add_ascendente);
+        ListaVinculada lista_vinculada1 = new ListaVinculada(nodo_raiz, add_ascendente);
 
         Nodo nodo1 = new Nodo(11);
         Nodo nodo2 = new Nodo(11);
         Nodo nodo3 = new Nodo(12);
 
-        lista_vinculada.addNodo(nodo1);
-        lista_vinculada.addNodo(nodo2);
-        lista_vinculada.addNodo(nodo3);
-        /* lista_vinculada.addNodo(10);
-        lista_vinculada.addNodo(11);
-        lista_vinculada.addNodo(11);
-        lista_vinculada.addNodo(12); */
-        lista_vinculada.imprimir();
+        lista_vinculada1.addNodo(nodo1);
+        lista_vinculada1.addNodo(nodo2);
+        lista_vinculada1.addNodo(nodo3);
+        /* lista_vinculada1.addNodo(10);
+        lista_vinculada1.addNodo(11);
+        lista_vinculada1.addNodo(11);
+        lista_vinculada1.addNodo(12); */
+        lista_vinculada1.imprimir();
 
-        lista_vinculada.removeNodoPorPosicion(0);
-        lista_vinculada.removeNodoPorNodo(nodo1);
+        lista_vinculada1.removeNodoPorPosicion(0);
+        lista_vinculada1.removeNodoPorNodo(nodo1);
         System.out.println();
-        lista_vinculada.imprimir();
-        System.out.println("Posicion de nodo3: " + lista_vinculada.getPosicionNodo(nodo3));
-        System.out.println("Cantidad de nodos: " + lista_vinculada.getCantidadNodos());
-        System.out.println("Nodo por posicion: " + lista_vinculada.getNodoPorPosicion(0));
+        lista_vinculada1.imprimir();
+        System.out.println("Posicion de nodo3: " + lista_vinculada1.getPosicionNodo(nodo3));
+        System.out.println("Cantidad de nodos: " + lista_vinculada1.getCantidadNodos());
+        System.out.println("Nodo por posicion: " + lista_vinculada1.getNodoPorPosicion(0));
 
-        for (Nodo nodo : lista_vinculada) {
+        for (Nodo nodo : lista_vinculada1) {
             System.out.println("Nodo: " + nodo);
         }
-        /* System.out.println(lista_vinculada.getNodoPorPosicion(0).getSiguienteNodo());
-        System.out.println(lista_vinculada.getNodoPorPosicion(0).getAnteriorNodo()); */
-        lista_vinculada.setComportamientoAdd(add_descendente);
-        lista_vinculada.imprimir();
+        /* System.out.println(lista_vinculada1.getNodoPorPosicion(0).getSiguienteNodo());
+        System.out.println(lista_vinculada1.getNodoPorPosicion(0).getAnteriorNodo()); */
+        lista_vinculada1.setComportamientoAdd(add_descendente);
+        lista_vinculada1.imprimir();
 
         Nodo nodo4 = new Nodo(14);
         Nodo nodo5 = new Nodo(9);
-        lista_vinculada.addNodo(nodo5);
+        lista_vinculada1.addNodo(nodo5);
         System.out.println();
-        lista_vinculada.imprimir();
+        lista_vinculada1.imprimir();
 
         System.out.println();
-        lista_vinculada.setComportamientoAdd(add_ascendente);
-        lista_vinculada.addNodo(nodo4);
-        lista_vinculada.imprimir();
+        lista_vinculada1.setComportamientoAdd(add_ascendente);
+        lista_vinculada1.addNodo(nodo4);
+        lista_vinculada1.imprimir();
+
+        /* Alumno alumno1 = new Alumno("esteban", "qatar", "41859294", 21);
+        Alumno alumno2 = new Alumno("valentina", "cuba", "44849294", 19);
+        Nodo nodo_alumno1 = new Nodo(alumno1);
+        Nodo nodo_alumno2 = new Nodo(alumno2);
+        ListaVinculada lista_vinculada2 = new ListaVinculada(nodo_alumno1, add_descendente); */
     }
 }
